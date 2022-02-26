103ecdbecccb8ae962d201522972ad7c316d797a
khushal <khushalsingh12@gmail.com>
2022-02-27 05:00:54 +0530

    added heroku entry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Apply database migrations"
 python manage.py migrate
 
-echo "runserver"
-python manage.py runserver 0.0.0.0:8000
+# echo "runserver"
+# python manage.py runserver 0.0.0.0:8000
