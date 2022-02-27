d94920c19d8cb78f92caaf0da61cdfb2a678a07d
khushal <khushalsingh12@gmail.com>
2022-02-27 07:06:49 +0530

    updated docker entry point

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,5 @@ echo "Apply database migrations"
 python manage.py migrate
 
 echo "runserver"
-# gunicorn --bind 0.0.0.0:80 drf_assignment.wsgi
-python manage.py runserver
+gunicorn --bind 0.0.0.0:$PORT drf_assignment.wsgi
+# python manage.py runserver
