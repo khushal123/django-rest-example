0472643c26ba602cf8b26ba3ce75adbac9ca7741
khushal <khushalsingh12@gmail.com>
2022-02-27 07:00:51 +0530

    updated heroku.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,5 @@ echo "Apply database migrations"
 python manage.py migrate
 
 echo "runserver"
-gunicorn --bind 0.0.0.0:80 drf_assignment.wsgi
+# gunicorn --bind 0.0.0.0:80 drf_assignment.wsgi
+python manage.py runserver
